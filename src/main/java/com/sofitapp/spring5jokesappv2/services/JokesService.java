8d42048f07a69173623c37e107d819126772adce
Karina Sofia Medina <sofi.k.medina@gmail.com>
2023-04-19 00:03:02 -0300

    fix-assigment
    Signed-off-by: Karina Sofia Medina <sofi.k.medina@gmail.com>

--- src/main/java/com/sofitapp/spring5jokesappv2/services/JokesService.java
+++ src/main/java/com/sofitapp/spring5jokesappv2/services/JokesService.java
@@ -1,9 +1,5 @@
 package com.sofitapp.spring5jokesappv2.services;
 
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.stereotype.Service;
-
-@Service
 public interface JokesService {
-    String getRandomJoke();
+    String getJoke();
 }
